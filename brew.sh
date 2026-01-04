1cb976b39a44821d9acee15cb0c2fd8b363b526b
Joe Laws <joe.laws@gmail.com>
2026-01-03 20:09:25 -0500

    Remove bash

--- brew.sh
+++ brew.sh
@@ -22,21 +22,9 @@ brew install moreutils
 brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
-# Install a modern version of Bash.
-brew install bash
-brew install bash-completion2
-
-# Switch to using brew-installed bash as default shell
-if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
-  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
-  chsh -s "${BREW_PREFIX}/bin/bash";
-fi;
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
-
-# Install GnuPG to enable PGP-signing commits.
-brew install gnupg
 
 # Install more recent versions of some macOS tools.
 brew install vim --with-override-system-vi
